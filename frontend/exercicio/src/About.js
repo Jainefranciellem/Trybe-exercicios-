557118dcd34cb9a084d67de7892c19e739c7bfa1
Jainefranciellem <jainefraciellen@gmail.com>
2022-11-21 16:23:13 -0300

    primeiros passos com react

---
+++ frontend/exercicio/src/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class helloWorld extends Component {
+  render() {
+    return (
+      <>
+        <h1>Fran</h1>
+        <p>uma desenvolvedora em desenvolvimento ^^</p>
+        <h2>Minhas habilidades</h2>
+        <ol>
+          <li>
+            aprendi JS assincrono e Jest assincrono em frontend
+          </li>
+        </ol>
+      </>
+    );
+  }
+}
+
+export default helloWorld;
